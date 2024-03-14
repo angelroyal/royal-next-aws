792655f8160bfee883f7c5436ebc9b5ca768ed11
bastiantinocoangel <47192223+bastiantinocoangel@users.noreply.github.com>
2024-03-14 11:30:54 -0500

    fix color

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import Navigation from "@/components/Navigation/Navigation";
-import LanguageProvider from "@/language/LanguageProvider";
-import { TokenProvider } from "@/config/context/AuthContext";
-import { CartAxiosProvider } from "@/components/Cart/CartAxios";
-import Token from "@/components/General/Token";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -17,17 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <LanguageProvider>
-          <TokenProvider>
-            <CartAxiosProvider>
-              <Token />
-              <Navigation />
-              {children}
-            </CartAxiosProvider>
-          </TokenProvider>
-        </LanguageProvider>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,25 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import "../assets/styles/web/App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import HomeHotel from "@/pages/Home/HomeHotel";
+import Token from "@/components/General/Token";
+import Navigation from "@/components/Navigation/Navigation";
+import LanguageProvider from "@/language/LanguageProvider";
+import { TokenProvider } from "@/config/context/AuthContext";
+import { CartAxiosProvider } from "@/components/Cart/CartAxios";
 
 export default function Home() {
-  return <HomeHotel />;
+  return (
+    <>
+      <LanguageProvider>
+        <TokenProvider>
+          <CartAxiosProvider>
+            <Token />
+            <Navigation />
+            <HomeHotel />
+          </CartAxiosProvider>
+        </TokenProvider>
+      </LanguageProvider>
+    </>
+  );
 }
