84c1d56f8f2dccd3b8f5bc22ee9238d2646b315b
LuisPerazaV <160287372+LuisPerazaV@users.noreply.github.com>
2024-03-20 11:22:28 -0500

    angelillos cardhotelT

--- src/services/Hotels/components/Listing/CardHotelT.jsx
+++ src/services/Hotels/components/Listing/CardHotelT.jsx
@@ -56,13 +56,14 @@ export default function CardHotelT() {
                 {/* NAME AND STARS HOTEL */}
                 <div>
                     <h2 className='text-fs-16 m-b mb-0'>Microtel inn and suites By Wyndham Culiacan</h2>
-                    <Rating name="read-only" value={5} readOnly size="small" className='!text-fs-12' />
+                    <Rating name="read-only" value={5} readOnly size="small" className='!text-fs-12 !gap-[2px]' />
                 </div>
 
                 <div className='flex max-md:flex-col'>
+
                     {/* INFO HOTEL*/}
-                    <div className='w-3/5  pr-[23px] max-md:w-full'>
-                        <div className='flex gap-1 '>
+                    <div className='w-3/5  pr-[23px] max-md:w-full flex flex-col gap-[8px]'>
+                        <div className='flex gap-1'>
                             <Image
                                 className="w-[11px] h-[14px]"
                                 src={`${process.env.NEXT_PUBLIC_URL}icons/location/location-bl.svg`}
@@ -72,7 +73,9 @@ export default function CardHotelT() {
                             />
                             <span className="text-bl-100 m-s-b text-fs-10">Culiacan, a 1km de Centro Histórico a 1km de Centro Histórico</span>
                         </div>
+
                         <span className='m-s-b text-fs-10 text-gry-100'>2 noches, 3 personas</span>
+
                         <p className='m-m text-fs-10 text-gry-70 !mb-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero alias repellendus saepe nam dignissimos inventore optio delectus soluta consectetur illo, nulla itaque eius ratione vel nemo animi illum reiciendis ea.</p>
 
                         <div className='flex bg-[#FFF0BD] px-[10px] py-[3px] items-center w-fit rounded-lg gap-1'>
@@ -83,23 +86,23 @@ export default function CardHotelT() {
                                 width={24}
                                 height={25}
                             />
-                            <span className='m-b text-fs-12 text-gry-100 '>{languageData.cardHotel.label}</span>
+                            <span className='m-b text-fs-12 text-gry-100 px-[8px]'>{languageData.cardHotel.label}</span>
                         </div>
                     </div>
 
                     {/* INFO PRICE */}
-                    <div className='w-2/5 border-l border-gry-70 flex flex-col pl-[23px] max-md:w-full max-md:border-t max-md:border-l-0 max-md:mt-2 max-md:pt-2 max-md:flex-row max-md:justify-between max-md:items-center max-md:pl-0'>
+                    <div className='w-2/5 border-l border-gry-70 flex flex-col pl-[23px] items-center max-md:w-full max-md:border-t max-md:border-l-0 max-md:mt-2 max-md:pt-2 max-md:flex-row max-md:justify-between max-md:pl-0'>
                         <div className='flex flex-col items-center '>
                             <span className='m-b text-red-100 text-fs-12'>¡{languageData.cardHotel.titleSpace} 3!</span>
                             <h2 className='m-b text-fs-16 flex items-center gap-1'><span className='text-fs-12'>{languageData.cartTour.from} </span>MXN $10,000</h2>
                             <div className='flex gap-1 mb-1 items-center'>
                                 <del className='m-s-b text-gry-100 text-fs-10'>MXN $11,000</del>
-                                <div className='bg-red-100 text-white text-fs-12 px-[5px] rounded-r-md'>10%</div>
+                                <div className='bg-red-100 text-white text-fs-12 px-[8px] py-[2px] rounded-r-md'>10%</div>
                             </div>
-                            <div className='m-b bg-[#E0FEF0] text-grn-100 text-fs-8 mb-3 py-[2px] px-[6px] rounded'>{languageData.cartTour.taxesText}</div>
+                            <div className='m-b bg-[#E0FEF0] text-grn-100 text-fs-8 mb-3 py-[4px] px-[8px] rounded'>{languageData.cartTour.taxesText}</div>
                         </div>
 
-                        <button className='bg-yw-100 text-black text-fs-12 m-b px-[40px] py-[5px] rounded-full hover:opacity-75 text-nowrap max-md:w-[160px] max-md:h-[32px] '>{languageData.cartTour.seeDetails}</button>
+                        <button className='bg-yw-100 text-black text-fs-12 m-b px-[40px] py-[5px] rounded-full hover:opacity-75 text-nowrap w-[160px] h-[32px]'>{languageData.cartTour.seeDetails}</button>
                     </div>
                 </div>
             </div>
