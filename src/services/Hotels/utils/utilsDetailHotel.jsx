da9de24d6c6d926fe5fc61457788dca6c37d2a80
GuilleJRL <148250562+GuilleJRL@users.noreply.github.com>
2024-04-01 10:31:43 -0500

    validation quere params date

--- src/services/Hotels/utils/utilsDetailHotel.jsx
+++ src/services/Hotels/utils/utilsDetailHotel.jsx
@@ -1,5 +1,3 @@
-import moment from "moment";
-
 export function formatAdultsAndChildren(adultChildren) {
   const adults = Math.floor(parseFloat(adultChildren));
   const children = Math.round((parseFloat(adultChildren) % 1) * 10);
@@ -19,8 +17,6 @@ export function parseQueryParams(url, codeHotel) {
   const type = queryParams.get("type");
   let checkIn = queryParams.get("check-in");
   let checkOut = queryParams.get("check-out");
-
-
   const occupanciesParam = queryParams.get("occupancies");
 
   let occupancies = [{ adults: 2, children: [] }];
@@ -29,22 +25,19 @@ export function parseQueryParams(url, codeHotel) {
   }
 
   const currentDate = new Date();
+  const checkInDate = new Date(checkIn);
 
-  if (
-    checkInDate <= currentDate ||
-    checkInDate - currentDate < 48 * 60 * 60 * 1000
-  ) {
-    console.log("entra");
+  if (checkInDate <= currentDate || checkInDate - currentDate < 2 * 24 * 60 * 60 * 1000) {
     const newCheckInDate = new Date(currentDate);
     newCheckInDate.setMonth(currentDate.getMonth() + 1);
-    checkIn = newCheckInDate.toISOString().split("T")[0];
+    checkIn = newCheckInDate.toISOString().split('T')[0];
   }
-  
+
   if (checkInDate <= currentDate || checkIn > checkOut) {
     const newCheckOutDate = new Date(currentDate);
     newCheckOutDate.setMonth(currentDate.getMonth() + 1);
     newCheckOutDate.setDate(currentDate.getDate() + 2);
-    checkOut = newCheckOutDate.toISOString().split("T")[0];
+    checkOut = newCheckOutDate.toISOString().split('T')[0];
   }
 
   return {
@@ -55,3 +48,4 @@ export function parseQueryParams(url, codeHotel) {
     occupancies,
   };
 }
+
