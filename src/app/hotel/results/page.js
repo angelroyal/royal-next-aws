e8aa45d3d30ee46c1a2293b4617dc26d546f1ed7
bastiantinocoangel <47192223+bastiantinocoangel@users.noreply.github.com>
2024-04-08 11:48:09 -0500

    fix

--- src/app/hotel/results/page.js
+++ src/app/hotel/results/page.js
@@ -8,22 +8,22 @@ import { TokenProvider } from "@/config/context/AuthContext";
 import { CartAxiosProvider } from "@/components/Cart/CartAxios";
 import Hotels from "@/services/Hotels/components/Listing/Hotels";
 
-export async function generateMetadata() {
-  const router = useRouter();
+// export async function generateMetadata() {
+//   const router = useRouter();
 
-  const { destination, 'check-in': checkIn, 'check-out': checkOut } = router.query;
+//   const { destination, 'check-in': checkIn, 'check-out': checkOut } = router.query;
 
-  const decodedDestination = decodeURIComponent(destination || '');
+//   const decodedDestination = decodeURIComponent(destination || '');
 
-  return {
-    title: `Resultados para ${decodedDestination} - Royal Vacations Mexico`,
-    description:
-    `Encuentra los mejores hoteles en ${decodedDestination} del ${checkIn} al ${checkOut}. Explora con Royal Vacations Mexico.`,
-    keywords:
-      "Políticas de cancelaciones, Procedimiento de cambios, Proteccion de Datos Personales",
-    author: "StayWuw",
-  };
-}
+//   return {
+//     title: `Resultados para ${decodedDestination} - Royal Vacations Mexico`,
+//     description:
+//     `Encuentra los mejores hoteles en ${decodedDestination} del ${checkIn} al ${checkOut}. Explora con Royal Vacations Mexico.`,
+//     keywords:
+//       "Políticas de cancelaciones, Procedimiento de cambios, Proteccion de Datos Personales",
+//     author: "StayWuw",
+//   };
+// }
 
 export default function Home() {
   return (
