23c95e0cb78b2669ba6139c206cb8e6523bcaf74
bastiantinocoangel <47192223+bastiantinocoangel@users.noreply.github.com>
2024-04-15 17:22:43 -0500

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -9,10 +9,43 @@ import { CartAxiosProvider } from "@/components/Cart/CartAxios";
 export default function Home() {
   const jsonLd = {
     "@context": "https://schema.org",
-    "@type": "Product",
-    name: "staywuwb",
-    image: "staywuwb",
-    description: "staywuwb",
+    "@type": "Corporation",
+    legalName: "StayWuw.com ",
+    url: "http://staywuw.com",
+    description:
+      "By investing in technology that takes the friction out of travel, Booking.com seamlessly connects millions of travelers to memorable experiences, a variety of transportation options, and incredible places to stay – from homes to hotels, and much more. As one of the world’s largest travel marketplaces for both established brands and entrepreneurs of all sizes, Booking.com enables properties around the world to reach a global audience and grow their businesses.",
+    address: {
+      "@type": "PostalAddress",
+      addressCountry: "Mexico",
+      addressRegion: "Mexico",
+      addressLocality: "Cancun, Mexico",
+      postOfficeBoxNumber: "150",
+      postalCode: "77505",
+      streetAddress: "Nader",
+    },
+    faxNumber: "+52 998134 2286",
+    logo: "https://sandboxmexico.com/assets/royal/principal-logo.svg",
+    founder: "Lic Pepe",
+    foundingDate: "1995",
+    foundingLocation: "Mexico",
+    areaServed: "Mexico",
+    brand: {
+      "@type": "Brand",
+      name: "http://staywuw.com/",
+    },
+    keywords:
+      "online travel agency, hotels, holiday rentals, flights, rental cars",
+    parentOrganization: {
+      "@type": "Corporation",
+      name: "Stay Wuw",
+    },
+    sameAs: [
+      "https://www.facebook.com/RoyalVacationsMx",
+      "https://www.instagram.com/royalvacationsmx/",
+      "https://twitter.com/bookingcom",
+      "https://www.linkedin.com/company/royal-vacations-mexico/",
+      "https://www.tiktok.com/@royalvacationsmx",
+    ],
   };
 
   return (
