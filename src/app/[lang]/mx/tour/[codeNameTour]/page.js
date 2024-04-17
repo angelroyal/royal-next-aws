340a27cac52515d9bd077eb61529cebe8a3e0229
bastiantinocoangel <47192223+bastiantinocoangel@users.noreply.github.com>
2024-04-17 13:34:48 -0500

    fix

--- src/app/[lang]/mx/tour/[codeNameTour]/page.js
+++ src/app/[lang]/mx/tour/[codeNameTour]/page.js
@@ -7,37 +7,6 @@ import { TokenProvider } from "@/config/context/AuthContext";
 import LanguageProvider from "@/language/LanguageProvider";
 import React from "react";
 
-export async function generateMetadata({ params, searchParams }) {
-  const requestBody = {
-    dateFrom: searchParams.dateStart,
-    days: 5,
-    provider: "ct",
-  };
-
-  try {
-    // METHOD AXIOS
-    const response = await axiosWithInterceptor.post(
-      `v1/activities/${params.codeNameTour}/availability`,
-      requestBody
-    );
-
-    // METADATA DETAIL HOTEL
-    const tourMetaData = response.data;
-
-    return {
-      title: `${tourMetaData.activity.name} - StayWuw`,
-      description: `${tourMetaData.activity.description}`,
-      address: tourMetaData.activity.address,
-      city: tourMetaData.activity.city,
-      destination: tourMetaData.activity.destination,
-    };
-  } catch (error) {
-    console.error("Error fetching hotel metadata:", error);
-    // Handle error here
-    return null;
-  }
-}
-
 export default async function DetailPageTour({ params, searchParams }) {
   const requestBody = {
     dateFrom: searchParams.dateStart,
@@ -63,7 +32,7 @@ export default async function DetailPageTour({ params, searchParams }) {
             <Navigation hotelDetails={true} />
             <div>
               {params.codeNameTour}
-              <div>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</div>
+              <div>PAAPAPAPAPAPAPAPAPAPAPAPAPAPAPAAP</div>
             </div>
             <Footer />
             {/* </DetailTourProvider> */}
