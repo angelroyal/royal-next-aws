4e27ab4cfe1a936f53f6948c54922f2327994d5b
LuisPerazaV <160287372+LuisPerazaV@users.noreply.github.com>
2024-04-18 13:36:41 -0500

    link btn contact

--- src/components/General/ContactUs.jsx
+++ src/components/General/ContactUs.jsx
@@ -15,32 +15,34 @@ export default function ContactUs() {
         const timer = setTimeout(() => {
             setDelayIcon(true);
         }, 100);
-        
+
         const contact = setTimeout(() => {
             setDelayContact(true);
         }, 400);
-        
+
         const bounce = setTimeout(() => {
             setDelayClass(true);
         }, 450);
 
 
-        return () => clearTimeout(timer , contact, bounce)
-        
+        return () => clearTimeout(timer, contact, bounce)
+
     }, []);
 
 
     return (
-        <>
+        <a
+            href="https://api.whatsapp.com/send?phone=529981342286&text=¡Hola!%20Necesito%20ayuda%20para%20planificar%20mi%20próximo%20viaje%20a%20México.%20¿Podrían%20orientarme%20sobre%20los%20mejores%20destinos%20y%20actividades%20que%20ofrecen?%20¡Espero%20su%20pronta%20respuesta!"
+            target="_blank"
+            rel="noopener noreferrer"
+            className='cursor-pointer'
+        >
             {delayContact &&
 
-                <a
+                <div
                     // className='fixed bottom-[2rem] right-[2rem] flex items-center gap-2 bg-white h-[50px] mb-[10px] w-fit rounded-tr-2xl rounded-l-3xl shadow-3xl cursor-pointer no-underline z-10 max-md:rounded-full hover:transform hover:scale-105 transition-transform duration-300 '
                     className={`fixed bottom-[2rem] right-[2rem] flex items-center gap-2 bg-white h-[50px] mb-[10px] w-fit rounded-tr-2xl rounded-l-3xl shadow-3xl cursor-pointer no-underline z-10 max-md:rounded-full ${delayClass ? "bounce2" : "animations-icon"
-                    }`}
-                    href="https://api.whatsapp.com/send?phone=529981342286&text=¡Hola!%20Necesito%20ayuda%20para%20planificar%20mi%20próximo%20viaje%20a%20México.%20¿Podrían%20orientarme%20sobre%20los%20mejores%20destinos%20y%20actividades%20que%20ofrecen?%20¡Espero%20su%20pronta%20respuesta!"
-                    target="_blank"
-                    rel="noopener noreferrer"
+                        }`}
                 >
                     <div className='bg-bl-100 w-[60px] h-[60px] rounded-full flex justify-center items-center '>
                     </div>
@@ -48,11 +50,11 @@ export default function ContactUs() {
                     <span className='m-b text-gry-100 pl-2 pr-6 text-fs-12 max-md:hidden'>{languageData.navBar.contact}</span>
 
 
-                </a>
+                </div>
             }
 
             {delayIcon &&
-                <div className={`bg-bl-100 p-3 rounded-full border border-white fixed bottom-[39px] right-[149px] z-[11]
+                <div className={`bg-bl-100 p-3 rounded-full border border-white fixed bottom-[39px] right-[149px] z-[11] max-md:right-[36px]
                     ${delayClass ? "bounce2" : "animations-icon"
                     }`}>
                     <Image
@@ -63,6 +65,6 @@ export default function ContactUs() {
                     />
                 </div>
             }
-        </>
+        </a>
     )
 }
