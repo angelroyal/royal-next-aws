0e88c07f4df0fed86b5a06567ee635bb1fad2188
bastiantinocoangel <47192223+bastiantinocoangel@users.noreply.github.com>
2024-04-19 16:01:25 -0500

    fix

--- src/app/[lang]/mx/[codeName]/tours/[id]/page.js
+++ src/app/[lang]/mx/[codeName]/tours/[id]/page.js
@@ -2,44 +2,42 @@
 
 import Token from "@/components/General/Token";
 import Footer from "@/components/Footer/Footer";
+import Page404 from "@/components/General/Page404";
 import LanguageProvider from "@/language/LanguageProvider";
 import Navigation from "@/components/Navigation/Navigation";
 import { TokenProvider } from "@/config/context/AuthContext";
 import Tour from "@/services/Tours/components/DetailTour/Tour";
 import { CartAxiosProvider } from "@/components/Cart/CartAxios";
-// import axiosWithInterceptor from "@/config/Others/axiosWithInterceptor";
-import { DetailTourProvider } from "@/services/Tours/context/DetailTourContext";
 import axiosWithInterceptor from "@/config/Others/axiosWithInterceptor";
-// import axios from "axios";
-import ErrorComponent from "@/components/General/ErrorComponent ";
+import { DetailTourProvider } from "@/services/Tours/context/DetailTourContext";
 
-// export async function generateMetadata({ params }) {
-//   try {
-//     // METHOD AXIOS
-//     const response = await axiosWithInterceptor.get(
-//       `v1/activities/${params.codeNameTour}`
-//     );
+export async function generateMetadata({ params }) {
+  try {
+    // METHOD AXIOS
+    const response = await axiosWithInterceptor.get(
+      `v1/activities/${params.id}`
+    );
 
-//     // METADATA DETAIL HOTEL
-//     const tourMetaData = response.data;
+    // METADATA DETAIL HOTEL
+    const tourMetaData = response.data;
 
-//     return {
-//       title: `${tourMetaData.activity.name} - StayWuw`,
-//       description: `${tourMetaData.activity.description}`,
-//       starRating: tourMetaData.activity.starRating,
-//       city: tourMetaData.destination.name,
-//     };
-//   } catch (error) {
-//     console.error("Error fetching hotel metadata:", error);
-//     // Handle error here
-//     return null;
-//   }
-// }
+    return {
+      title: `${tourMetaData.activity.name} - StayWuw`,
+      description: `${tourMetaData.activity.description}`,
+      starRating: tourMetaData.activity.starRating,
+      city: tourMetaData.destination.name,
+    };
+  } catch (error) {
+    console.error("Error fetching hotel metadata:", error);
+    // Handle error here
+    return null;
+  }
+}
 
 export default async function DetailPageTour({ params }) {
   try {
     const response = await axiosWithInterceptor.get(
-      `https://api.sandboxmexico.com/api/es/v1/activities/paddle-surf-en-el-islote-el-morro`
+      `v1/activities/${params.id}`
     );
 
     const tourMetaData = response.data;
@@ -63,7 +61,6 @@ export default async function DetailPageTour({ params }) {
       </LanguageProvider>
     );
   } catch (error) {
-    console.error("Error fetching hotel data:", error);
     return (
       <LanguageProvider>
         <TokenProvider>
@@ -71,43 +68,7 @@ export default async function DetailPageTour({ params }) {
             <DetailTourProvider>
               <Token />
               <Navigation hotelDetails={true} />
-              <ErrorComponent
-                message={`Failed to load tour data. Error: ${error}`}
-              />
-              <div
-                style={{
-                  margin: "20px",
-                  padding: "20px",
-                  backgroundColor: "#ffcccc",
-                  color: "#333",
-                  borderRadius: "8px",
-                  textAlign: "left",
-                }}
-              >
-                <h2>Error Occurred</h2>
-                {error.response ? (
-                  <>
-                    <h4>Response:</h4>
-                    <p>Status: {error.response.status}</p>
-                    <p>Headers:</p>
-                    <pre>{JSON.stringify(error.response.headers, null, 2)}</pre>
-                    <p>Data:</p>
-                    <pre>{JSON.stringify(error.response.data, null, 2)}</pre>
-                  </>
-                ) : error.request ? (
-                  <>
-                    <h4>Request:</h4>
-                    <p>No response received. Request details unavailable.</p>
-                  </>
-                ) : (
-                  <>
-                    <h4>Error Message:</h4>
-                    <p>{error.message}</p>
-                  </>
-                )}
-                <p>Config:</p>
-                <pre>{JSON.stringify(error.config, null, 2)}</pre>
-              </div>
+              <Page404 />
               <Footer />
             </DetailTourProvider>
           </CartAxiosProvider>
