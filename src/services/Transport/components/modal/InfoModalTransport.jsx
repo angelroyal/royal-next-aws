acfbaaa898611b1d78fb82b26b89fd11118edd20
bastiantinocoangel <47192223+bastiantinocoangel@users.noreply.github.com>
2024-04-30 17:39:08 -0500

    fix

--- src/services/Transport/components/modal/InfoModalTransport.jsx
+++ src/services/Transport/components/modal/InfoModalTransport.jsx
@@ -1,36 +1,33 @@
-import React, { useState } from 'react'
 import "@/assets/styles/general/Swiper.css";
 
-export default function InfoModalTransport() {
+import React, { useContext, useState } from "react";
 
-  const [changePrivate, setChangePrivate] = useState(true);
+// import ModalTransportContext from "../../context/ModalTransportContext";
+import DateAndHour from "./DateAndHour";
+import PassengersTransport from "./PassengersTransport";
 
-  const [counter, setCounter] = useState(0);
-
-  const counterAdd = () => {
-    setCounter(counter + 1)
-  }
-
-  const counterRemove = () => {
-    if (counter > 0)
-      setCounter(counter - 1)
-  }
-
+export default function InfoModalTransport(props) {
+  const { transport } = props;
+  console.log(transport);
 
   return (
-    <div className='flex flex-col gap-2 w-1/2 max-md:w-full'>
+    <div className="flex flex-col gap-2 w-1/2 max-md:w-full">
       {/* TITLE */}
-      <h3 className='text-fs-24 m-b mb-[36px]'>Reservar transporte</h3>
+      <h3 className="text-fs-24 m-b mb-[36px]">
+        ¡Ya casi está listo tu transporte!
+      </h3>
 
       {/* TYPE OF TRIP */}
-      <div className='flex flex-col gap-[6px] mb-[16px]'>
-        <span className='text-gry-100 text-fs-12 m-s-b'>Tipo de viaje</span>
+      <div className="flex flex-col gap-[6px] mb-[16px]">
+        <span className="text-gry-100 text-fs-12 m-s-b">Tipo de viaje</span>
 
-        <div className='text-gry-100 text-fs-12 m-s-b py-[8px] px-[16px] bg-gry-50 rounded-full w-fit'>Redondo</div>
+        <div className="text-gry-100 text-fs-12 m-s-b py-[8px] px-[16px] bg-gry-50 rounded-full w-fit">
+          {transport.round === true ? "Redondo" : "Sencillo"}
+        </div>
       </div>
 
       {/* FROM , TO */}
-      <span className='text-gry-100 text-fs-12 m-s-b'>Desde</span>
+      <span className="text-gry-100 text-fs-12 m-s-b">Desde</span>
 
       <div className="flex bg-[#f1f6ff] rounded-[4px] gap-[8px] py-[10px] pl-[16px] max-w-[324px] mb-[16px]">
         <img
@@ -39,10 +36,12 @@ export default function InfoModalTransport() {
           className="w-[12.7] h-[16px] mr-2"
         />
 
-        <div className='text-gry-100 m-s-b text-fs-12'>Aeropuesto internacional de cancun</div>
+        <div className="text-gry-100 m-s-b text-fs-12">
+          Aeropuesto internacional de cancun
+        </div>
       </div>
 
-      <span className='text-gry-100 text-fs-12 m-s-b'>Hacia</span>
+      <span className="text-gry-100 text-fs-12 m-s-b">Hacia</span>
 
       <div className="flex bg-[#f1f6ff] rounded-[4px] gap-[8px] py-[10px] pl-[16px] max-w-[324px]  mb-[16px]">
         <img
@@ -51,91 +50,16 @@ export default function InfoModalTransport() {
           className="w-[12.7] h-[16px] mr-2"
         />
 
-        <div className='text-gry-100 m-s-b text-fs-12'>Hotel nickelodeon Riviera maya</div>
+        <div className="text-gry-100 m-s-b text-fs-12">
+          Hotel nickelodeon Riviera maya
+        </div>
       </div>
 
       {/* DATE AND HOUR */}
-      <div className='flex w-full gap-[24px] mb-[36px]'>
-
-        {/* DATE */}
-        <div className='w-1/2'>
-          <span>Fecha</span>
-
-          <div className='flex px-[16px] py-[11.5px] border border-[#ebebeb] items-center gap-2 relative'>
-            <img
-              className="w-[14px] h-[16px]"
-              src={`${process.env.NEXT_PUBLIC_URL}icons/calendar/calendar-b.svg`}
-              alt={`${process.env.NEXT_PUBLIC_NAME_COMPANY} icon calendar`}
-            />
-            {/* <span className='text-fs-12 m-m'>23/05/2024</span> */}
-            <input type="date" className='focus:outline-0 bg-white text-fs-12 m-m time-input'></input>
-          </div>
-        </div>
-
-        {/* HOUR */}
-        <div className='w-1/2'>
-          <div>Horario</div>
-          <div className='flex py-[11.5px] px-[16px] border border-[#ebebeb] items-center gap-2 relative'>
-            <img
-              className=""
-              src={`${process.env.NEXT_PUBLIC_URL}icons/general/schedule.svg`}
-              alt={`${process.env.NEXT_PUBLIC_NAME_COMPANY} icon calendar`}
-            />
-            <input type="time" id="timeInput" className="focus:outline-0 bg-white text-fs-12 m-m time-input" />
-            {/* <span className='text-fs-12 m-m'>09:00 am</span> */}
-          </div>
-        </div>
-      </div>
+      <DateAndHour transport={transport} />
 
       {/* HOW MANY PASSENGERS */}
-      <div className='flex flex-col gap-[16px]'>
-        <span className='text-fs-14 m-s-b'>¿Cuántos pasajeros?</span>
-
-        <div className='flex gap-[16px]'>
-          <div
-            onClick={counterRemove}
-            className={`${counter === 0
-                ? "cursor-not-allowed"
-                : "cursor-pointer"
-              } px-[8px] flex items-center`}
-          >
-            <img
-              src={`${process.env.NEXT_PUBLIC_URL}icons/remove/remove-100.svg`}
-              alt="icon Remove"
-              width={9.3}
-              height={9.3}
-            />
-          </div>
-
-          <div className='pl-[22px] pr-[22px] py-[8px] border border-gry-50'>
-            {counter}
-          </div>
-
-          <button
-            onClick={counterAdd}
-            className='px-[8px] cursor-no-drop'
-          >
-            <img
-              src={`${process.env.NEXT_PUBLIC_URL}icons/add/add-100.svg`}
-              alt="icon Add"
-              width={9.3}
-              height={9.3}
-            />
-          </button>
-
-        </div>
-
-        {changePrivate === true &&
-          <div className='flex gap-1 items-center'>
-            <span className='text-fs-12 m-m'>Precio individual: </span>
-            <span className='text-fs-12 m-b text-bl-100'>MXN</span>
-            <span className='text-fs-14 m-b text-bl-100'>$5,000</span>
-          </div>
-        }
-
-
-      </div>
-
+      <PassengersTransport transport={transport} />
     </div>
-  )
+  );
 }
---
+++ src/services/Transport/components/modal/TransportPriceCart.jsx
@@ -0,0 +1,119 @@
+import React, { useContext, useState } from "react";
+
+import CancelPolicyTransportWhite from "../ToulTip/CancelPolicyTransportWhite";
+import ModalTransportContext from "../../context/ModalTransportContext";
+import { saveToCartTransport } from "../../Api/requestTransport";
+
+export default function TransportPriceCart(props) {
+  const { transport } = props;
+  const {
+    passenger,
+    departureDate,
+    comebackDate,
+    departureTime,
+    comebackTime,
+  } = useContext(ModalTransportContext);
+  console.log(transport);
+
+  const [openPolicy, setOpenPolicy] = useState(false);
+
+  const priceShared = transport.price * passenger;
+
+  const handleReserveNow = async () => {
+    try {
+      const uidCart = localStorage.getItem("uid-cart");
+      let cartId = "";
+
+      if (uidCart) {
+        const { uid } = JSON.parse(uidCart);
+        cartId = uid;
+      }
+
+      // REQUEST BODY ADD CART
+      const saveRequestCart = {
+        from: 212,
+        to: 211,
+        pax: passenger,
+        vehicleId: transport.id,
+        vehicleLabel: transport.label,
+        departureDate: departureDate,
+        departureTime: departureTime,
+        comebackDate: comebackDate,
+        comebackTime: comebackTime,
+        price: transport.price,
+        currency: "MXN",
+        destinationId: 719395,
+        cancelPolicyHours: transport.cancellation,
+      };
+
+      // IF UID EXIST ADD UID
+      if (cartId) {
+        saveRequestCart.cartId = cartId;
+      }
+
+      //   AXIOS POST ADD CART
+      const response = await saveToCartTransport(saveRequestCart);
+
+      const cartUid = response.cart;
+      const expirationTime = new Date().getTime() + 2 * 60 * 60 * 1000;
+      localStorage.setItem(
+        "uid-cart",
+        JSON.stringify({ uid: cartUid, expirationTime })
+      );
+      //   fetchData(cartUid);
+      setTimeout(() => {
+        router.push(`/${language}/booking?uid=${cartUid}`);
+      }, 2000);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-[8px]">
+      <div className="flex justify-between items-center">
+        <span className="m-b text-fs-14">Total:</span>
+
+        <span className="m-b text-fs-14">
+          MXN{" "}
+          {transport.type === "shared" ? (
+            <span className="text-fs-20">
+              ${" "}
+              {Math.floor(priceShared)
+                .toLocaleString("es-MX", { currency: "MXN" })
+                .replace(".00", "")}
+              .<sup>{(priceShared % 1).toFixed(2).slice(2)}</sup>
+            </span>
+          ) : (
+            <span className="text-fs-20">
+              ${" "}
+              {Math.floor(transport.price)
+                .toLocaleString("es-MX", { currency: "MXN" })
+                .replace(".00", "")}
+              .<sup>{(transport.price % 1).toFixed(2).slice(2)}</sup>
+            </span>
+          )}
+        </span>
+      </div>
+
+      <button
+        className="py-[14px] bg-bl-100 text-white m-b text-fs-12 hover:bg-bl-110 rounded-full text-center"
+        onClick={handleReserveNow}
+      >
+        Agregar al carrito
+      </button>
+
+      <div
+        onMouseOver={() => setOpenPolicy(true)}
+        onMouseLeave={() => setOpenPolicy(false)}
+        className="text-center"
+      >
+        <span className="relative text-center text-fs-14 m-s-b text-bl-100 cursor-pointer relative">
+          {/* {languageData.containerModalHotel.policies} */}
+          politicas de cancelacion
+          {openPolicy === true && <CancelPolicyTransportWhite />}
+        </span>
+      </div>
+    </div>
+  );
+}
