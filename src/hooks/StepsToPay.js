7de3c07f0b641fa3c55f76fe06419dc22d2c5307
GuilleJRL <148250562+GuilleJRL@users.noreply.github.com>
2024-05-03 12:27:28 -0500

    validation steps

--- src/hooks/StepsToPay.js
+++ src/hooks/StepsToPay.js
@@ -22,8 +22,10 @@ export function StepsToPayments({ step, handleStepChange }) {
   const { languageData } = useContext(LanguageContext);
 
   const handleStepsButton = (stepValue) => {
-    if (stepValue < 3) {
-      handleStepChange(stepValue);
+    if (step < 3) {
+      if (stepValue < 3) {
+        handleStepChange(stepValue);
+      }
     }
   };
 
@@ -40,7 +42,9 @@ export function StepsToPayments({ step, handleStepChange }) {
                 : "bg-gry-50 text-gry-70 before:border-gry-50"
             } ${stepValue.value === 1 ? "w-[191px]" : "w-[228px]"} ${
               stepValue.value === 3 && "!cursor-default"
-            }`}
+            }
+            
+            ${step === 3 && "!cursor-default"}`}
           >
             <span
               className={`absolute left-0 p-2 w-[44px] h-[44px] m-s-b text-fs-14 flex justify-center items-center ${
@@ -72,10 +76,13 @@ export function StepsToPaymentsM({ step, handleStepChange }) {
   const { languageData } = useContext(LanguageContext);
 
   const handleStepsButton = (stepValue) => {
-    if (stepValue < 3) {
-      handleStepChange(stepValue);
+    if (step < 3) {
+      if (stepValue < 3) {
+        handleStepChange(stepValue);
+      }
     }
   };
+  
   return (
     <div className="flex items-center justify-center">
       {Steps.map((stepValue, index) => (
@@ -87,7 +94,7 @@ export function StepsToPaymentsM({ step, handleStepChange }) {
           >
             <div
               className={`border-2 p-2 w-[32px] h-[32px] flex items-center justify-center m-s-b text-fs-14 bg-white rounded-full ${
-                step && step === stepValue.value
+                step && step >= stepValue.value
                   ? "border-bl-100 text-or-100"
                   : "border-gry-70 text-gry-70"
               }`}
@@ -97,7 +104,7 @@ export function StepsToPaymentsM({ step, handleStepChange }) {
 
             <span
               className={`m-s-b text-fs-12 ${
-                step && step === stepValue.value ? "text-bl-100" : "text-gry-70"
+                step && step >= stepValue.value ? "text-bl-100" : "text-gry-70"
               }`}
             >
               {languageData.payment[stepValue.label]}
@@ -107,7 +114,7 @@ export function StepsToPaymentsM({ step, handleStepChange }) {
           {Steps.length - 1 !== index && (
             <span
               className={`border-[1px] border-dashed w-[39px] ${
-                step && step === stepValue.value
+                step && step >= stepValue.value
                   ? "border-bl-100"
                   : "border-gry-70"
               }`}
