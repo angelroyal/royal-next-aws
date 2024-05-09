c87495ce5763d8654b8d9c6e67d3b5cbc73fb921
bastiantinocoangel <47192223+bastiantinocoangel@users.noreply.github.com>
2024-05-09 10:19:30 -0500

    fix

---
+++ src/app/home/page.js
@@ -0,0 +1,23 @@
+import Token from "@/components/General/Token";
+import Footer from "@/components/Footer/Footer";
+import ContactUs from "@/components/General/ContactUs";
+import LanguageProvider from "@/language/LanguageProvider";
+import Navigation from "@/components/Navigation/Navigation";
+import { TokenProvider } from "@/config/context/AuthContext";
+import { CartAxiosProvider } from "@/components/Cart/CartAxios";
+import HomeHotel from "@/services/Hotels/components/home/HomeHotel";
+
+export default function Home() {
+  return (
+    <LanguageProvider>
+      <TokenProvider>
+        <CartAxiosProvider>
+          <Token />
+          <Navigation />
+
+aqui pinche luis          <Footer />
+        </CartAxiosProvider>
+      </TokenProvider>
+    </LanguageProvider>
+  );
+}
