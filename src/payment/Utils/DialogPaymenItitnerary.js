46bfbc1e12397d54f3b64e94982a4a4e03cb23f8
GuilleJRL <148250562+GuilleJRL@users.noreply.github.com>
2024-05-14 17:16:07 -0500

    itinerary skeleton

--- src/payment/Utils/DialogPaymenItitnerary.js
+++ src/payment/Utils/DialogPaymenItitnerary.js
@@ -9,7 +9,6 @@ import { ShareContainer } from "../../utils/booking/ShareContainer";
 import { StepperContext } from "../context/steeperContext";
 import { DialogItineraryMobile } from "../itinerary/others/DialogItineraryMobile";
 
-import IconShowLess from "../../assets/icons/hotel/modal/show_less.svg";
 import IconRightBl from "../../assets/icons/utils/payment/right-bl.svg";
 import IconShowLessW from "../../assets/icons/utils/payment/show_less_w.svg";
 import IconShareW from "../../assets/icons/utils/payment/share-w.svg";
@@ -45,36 +44,43 @@ export function DialogPaymentItinerary(props) {
   return (
     <>
       {reservationData && (
-        <div className="box-inferior booking-container">
+        <div className="lg:hidden border-t-0 bg-white sticky bottom-0 left-0 w-full h-auto ">
           {step !== 3 ? (
-            <div className="container-info-mobile-itinerary">
+            <div className="rounded-t-[12px] relative h-full pt-[1.2rem] pb-[1rem] px-[1.2rem] shadow-[0px_-4px_14px_-3px_rgb(0,0,0,52%)]">
               <div
-                className="circle-open-dialog-m"
+                className="absolute top-[-16px] w-[3rem] h-[3rem] rounded-full left-0 right-0 mx-auto bg-white z-[3] shadow-[0px_-12px_12px_-11px]"
                 onClick={() => setOpenDialog(true)}
               >
-                <Image className="icon-show-less !w-1/2" src={IconShowLess} width={14} height={7}/>
+                <Image
+                  className="absolute top-[13px] left-[11px] !w-1/2"
+                  src={`${process.env.NEXT_PUBLIC_URL}icons/arrows/up-70.svg`}
+                  width={14}
+                  height={7}
+                  alt="show less icon"
+                />
               </div>
 
-              <div className="d-flex flex-column ps-2 pe-2">
-                <div className="type-reservation-payment-m">
+              <div className="flex flex-col px-2">
+                <div className="block pb-3 border-b border-gry-70">
+                  <p className="mb-2 m-s-b text-fs-12 "></p>
                   {
                     languageData.navigation[
                       reservationType(reservationData.items[0].type)
                     ]
                   }
 
-                  <span className="reservation-name-m">
+                  <p className="text-fs-10 text-gry-70 m-s-b m-0">
                     {reservationData.items[0].name}
-                  </span>
+                  </p>
                 </div>
 
                 {/* Dialog  */}
                 <div className="d-flex justify-content-between mt-3">
-                  <div className="total-price-reservation-m">Total</div>
+                  <div className="text-fs-16 m-b ">Total</div>
 
-                  <div className="m-i-price-c">
-                    <span>MXN</span>
-                    <span className="price-number-itinerary-m">
+                  <div className="m-b text-black">
+                    <span className="text-fs-14 mr-[8px]">MXN</span>
+                    <span className="text-fs-20">
                       $
                       {Math.floor(reservationData.summary.totalCurrentPrice)
                         .toLocaleString("es-MX", { currency: "MXN" })
@@ -107,10 +113,10 @@ export function DialogPaymentItinerary(props) {
                   {step === 1 && (
                     <button
                       onClick={() => handleStepChange(step + 1)}
-                      className="next-steep-button-itinerary"
+                      className="py-[10px] px-[2.1rem] flex items-center justify-center bg-yw-100 border-0 rounded-full m-b text-nowrap gap-x-[0.3rem] text-fs-10"
                     >
                       {languageData.itinerary.detailsPayment.completePayment}
-                      <Image src={IconRightBl} />
+                      <Image src={`${process.env.NEXT_PUBLIC_URL}icons/arrows/right-100.svg`} width={6} height={13} alt="arrow right icon"/>
                     </button>
                   )}
 
