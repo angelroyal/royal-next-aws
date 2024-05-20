a7b87157ad8f0337e2c240aa4165bb910ba8b274
GuilleJRL <148250562+GuilleJRL@users.noreply.github.com>
2024-05-20 16:58:02 -0500

    details itinerary

--- src/payment/itinerary/Itinerary.js
+++ src/payment/itinerary/Itinerary.js
@@ -8,7 +8,6 @@ import { AlertNoAvailability, AlertUpdate } from "../Booking/AlertRate";
 
 import { isAnyHotelUnavailable } from "../config/itineraryHelpers";
 
-import IconRoyal from "../../assets/icons/utils/payment/icon-royal-vacations.svg";
 import { BookingContext } from "../context/BookingContext";
 
 export default function Itinerary(props) {
@@ -34,14 +33,16 @@ export default function Itinerary(props) {
   return (
     <div className="m-scroll-i">
       <div className="display-title-button-share">
-        <div className="m-fit d-flex">
+        <div className="flex !gap-x-2 w-full items-start justify-start items-start !mb-2">
           {/* <IconRoyal className="icon-royal-itinerary" /> */}
           <Image
-            className="icon-royal-itinerary"
-            src={IconRoyal}
+            className="w-[27px] h-[25px]"
+            src={`${process.env.NEXT_PUBLIC_URL}icons/general/infotipo-staywuw.svg`}
             alt={`${process.env.NEXT_PUBLIC_NAME_COMPANY} icon`}
+            width={27}
+            height={25}
           />
-          <h1 className="itinerary-title-page">
+          <h1 className="text-fs-24 m-b text-black">
             {languageData.itinerary.titleItinerary}
           </h1>
         </div>
