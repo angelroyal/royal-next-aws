fcfc51069caa17c73bd5f1b533d68b3f420bb432
devs <devs@royalvacationsmexico.com>
2024-06-05 10:21:14 -0500

    fix route

--- src/app/[lang]/mx/[codeName]/transport/page.js
+++ src/app/[lang]/mx/[codeName]/transport/page.js
@@ -55,6 +55,7 @@ export default async function DetailPageHotel({ searchParams }) {
       </LanguageProvider>
     );
   } catch (error) {
+    console.log(error);
     return (
       <LanguageProvider>
         <TokenProvider>
