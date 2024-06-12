c638e46d18e190831e7e56a9b2438c6a53e1803a
LuisPerazaV <160287372+LuisPerazaV@users.noreply.github.com>
2024-06-12 12:10:26 -0500

    swiper gallery tour pagination

--- src/services/Tours/components/DetailTour/GalleryTour.jsx
+++ src/services/Tours/components/DetailTour/GalleryTour.jsx
@@ -1,23 +1,27 @@
 "use client";
 
-import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-
 import "swiper/css";
 import "swiper/css/navigation";
-
-import { Navigation } from "swiper/modules";
-
+import "swiper/css/pagination";
+import "../../../../assets/styles/general/Swiper.css";
 import "../../../../assets/css/Hotel/GalleryImage.css";
+
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper/modules";
+
 
 export default function GalleryTour({ images }) {
   return (
     <>
       <Swiper
         navigation={true}
-        modules={[Navigation]}
-        className="mySwiper h-[20rem] md:h-[22rem] xl:h-[30rem] mt-4 rounded-lg
-     "
+        modules={[Pagination, Navigation]}
+        pagination={{
+          clickable: true,
+        }}
+        id="swiper-gallery-tour"
+        className="mySwiper h-[20rem] md:h-[22rem] xl:h-[30rem] mt-4 rounded-lg"
       >
         {images.map((image, index) => (
           <SwiperSlide key={index}>
