10effae5c1222962afaaf4de29941321fccb7beb
LuisPerazaV <160287372+LuisPerazaV@users.noreply.github.com>
2024-06-12 13:48:18 -0500

    select listing hotel,tour,transport iphone

--- src/services/Tours/components/Listing/OrderingTour.js
+++ src/services/Tours/components/Listing/OrderingTour.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React, { useContext } from "react";
 
 import LanguageContext from "@/language/LanguageContext";
@@ -17,7 +18,7 @@ export default function OrderingTour() {
   return (
     <>
       {orderingTour.items.length > 0 && (
-        <div className="relative">
+        <div className="relative flex">
           <label
             htmlFor="location"
             className="m-m text-gry-100 text-fs-10 absolute top-[12px] left-[18px] "
@@ -25,7 +26,7 @@ export default function OrderingTour() {
             {languageData.filtersHotel.order}
           </label>
           <select
-            className="pt-[1.2rem] mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-bl-70 sm:text-sm sm:leading-6 text-fs-14"
+            className="pt-[1.2rem] mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300  appearance-none focus:outline-none sm:text-sm sm:leading-6 text-fs-14"
             value={orderTour}
             onChange={(event) => handleOrderingFilters(event)}
           >
@@ -35,6 +36,13 @@ export default function OrderingTour() {
               </option>
             ))}
           </select>
+          <Image
+            src={`${process.env.NEXT_PUBLIC_URL}icons/arrows/down-100.svg`}
+            width={12}
+            height={12}
+            alt="icon-arrow down"
+            className="h-3 w-3 absolute right-[20px] top-[26px]"
+          />
         </div>
       )}
     </>
