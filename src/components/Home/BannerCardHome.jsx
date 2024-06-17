9c8359eee4b14353fb3336e3699521afd1e735f5
GuilleJRL <148250562+GuilleJRL@users.noreply.github.com>
2024-06-17 16:12:06 -0500

    shuffle home

--- src/components/Home/BannerCardHome.jsx
+++ src/components/Home/BannerCardHome.jsx
@@ -1,22 +1,34 @@
+import { useContext } from "react";
+import { TotalStars } from "../General/Stars";
+import LanguageContext from "@/language/LanguageContext";
 
-import { TotalStars } from '../General/Stars'
+export default function BannerCardHome({ hotel }) {
+  const { language, languageData } = useContext(LanguageContext);
 
-export default function BannerCardHome() {
-    return (
-        <div className="absolute flex justify-between w-full bottom-0">
+  return (
+    <div
+      className="absolute flex justify-between w-full bottom-0"
+    >
+      <div className="text-fs-20 m-s-b text-white pl-[24px] w-[250px] items-center flex">
+        {hotel[language].destination}
+      </div>
 
-            <div className="text-fs-20 m-s-b text-white pl-[24px] w-[250px] items-center flex">Lorem ipsum dolor sit amet</div>
+      <div className="bg-white rounded-tl-lg px-[16px] py-[8px] w-[189px]">
+        <h4 className="text-fs-12 m-m">{hotel.name}</h4>
+        <TotalStars stars={5} width={"w-[11]"} height={"h-[11]"} />
 
-            <div className="bg-white rounded-tl-lg px-[16px] py-[8px] w-[189px]">
-                <h4 className="text-fs-12 m-m">Hotel Nickelodeon riviera maya</h4>
-                <TotalStars
-                    stars={5}
-                    width={"w-[11]"}
-                    height={"h-[11]"}
-                />
-
-                <span className="flex m-b text-fs-10 gap-2 items-center">desde <span className="text-fs-16"> MXN $10,000</span></span>
-            </div>
-        </div>
-    )
+        <span className="flex m-b text-fs-10 gap-2 items-center">
+          {languageData.cartTour.from}{" "}
+          <span className="text-fs-16">
+            {" "}
+            MXN{" "}
+            {Math.floor(hotel.price)
+              .toLocaleString("es-MX", { currency: "MXN" })
+              .replace(".00", "")}
+            .<sup>{(hotel.price % 1).toFixed(2).slice(2)}</sup>
+          </span>
+        </span>
+      </div>
+    </div>
+  );
 }
