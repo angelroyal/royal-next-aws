62093e9b27290b4fce4b3f564019bd9e974aad25
LuisPerazaV <160287372+LuisPerazaV@users.noreply.github.com>
2024-06-18 16:14:53 -0500

    btn recommendations transport

--- src/components/Recommended/Transport/TransportRecommendation.jsx
+++ src/components/Recommended/Transport/TransportRecommendation.jsx
@@ -4,7 +4,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "@/assets/styles/general/Swiper.css";
 
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from "react";
 import { Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -13,81 +13,93 @@ import { getCatalogueTransport } from "../Api/RequestRecommendation";
 import { CatalogueRoutesCard } from "@/services/Transport/Home/RecommendedTransport";
 import CardGalleryTransportSkeleton from "@/components/Skeleton/CardGalleryTransportSkeleton";
 export default function TransportRecommendation() {
-    
-    const { languageData } = useContext(LanguageContext);
+  const { languageData , language } = useContext(LanguageContext);
 
-    const [transportCatalogs, setTransportCatalogs] = useState([]);
+  const [transportCatalogs, setTransportCatalogs] = useState([]);
 
-    useEffect(() => {
-      const responseCatalogue = async ()=>{
-        const response = await getCatalogueTransport();
-        // console.log(response);
-        if(response){
-          setTransportCatalogs(response.data);
-        }
+  const changeTransport = () => {
+    window.open(`/${language}/transports`, "_self");
+  };
+
+  useEffect(() => {
+    const responseCatalogue = async () => {
+      const response = await getCatalogueTransport();
+      // console.log(response);
+      if (response) {
+        setTransportCatalogs(response.data);
       }
-      responseCatalogue();
-    }, []);
+    };
+    responseCatalogue();
+  }, []);
 
+  return (
+    <div className="bg-white rounded-lg p-[36px] relative">
+      <div className="flex justify-between items-center mb-[36px] max-md:flex-col max-md:items-start gap-3">
+        {/* TITLE */}
+        <span className="text-fs-20 m-b max-md:text-fs-16">
+          {languageData.recommendations.transport.titleRecommedation}
+        </span>
 
-    return (
+        {/* BTN CHOOSE TRANSPORT */}
+        <button
+          onClick={() => {
+            changeTransport();
+          }}
+          className="m-s-b bg-bl-100 text-white text-fs-12 rounded-full py-[14px] px-[16px] hover:bg-bl-110"
+        >
+          {languageData.recommendations.transport.btnchoose}
+        </button>
+      </div>
 
-        <div className='bg-white rounded-lg p-[36px] relative'>
-
-            <div className='flex justify-between items-center mb-[36px] max-md:flex-col max-md:items-start gap-3'>
-
-                {/* TITLE */}
-                <span className='text-fs-20 m-b max-md:text-fs-16'>{languageData.recommendations.transport.titleRecommedation}</span>
-
-                {/* BTN CHOOSE TRANSPORT */}
-                <button className='m-s-b bg-bl-100 text-white text-fs-12 rounded-full py-[14px] px-[16px] hover:bg-bl-110'>{languageData.recommendations.transport.btnchoose}</button>
-            </div>
-
-            <Swiper
-                slidesPerView={4}
-                spaceBetween={12}
-                id="swiper-tour-recommendation"
-                navigation
-                modules={[Navigation]}
-                className="mySwiper !static mb-0 h-[22rem]"
-                breakpoints={{
-                    0: {
-                        slidesPerView: 1,
-                    },
-                    500: {
-                        slidesPerView: 1.5,
-                    },
-                    768: {
-                        slidesPerView: 2.5,
-                    },
-                    1024: {
-                        slidesPerView: 3.5,
-                    },
-                    1280: {
-                        slidesPerView: 4.5,
-                    },
-                    1536: {
-                        slidesPerView: 5,
-                    },
-                }}
-            >
-                {/* CART TRANSPORT */}
-                {transportCatalogs.length > 0 
-                ? transportCatalogs.map((transportData, index) => (
-                    <SwiperSlide key={index} className="!rounded-lg">
-                        <div className="flex flex-col gap-2 p-[16px] rounded-lg bg-white shadow-3xl">
-                            <CatalogueRoutesCard transport={transportData} recommended={true}/>
-                        </div>
-                    </SwiperSlide>
-                )):
-                [...Array(5)].map((_, index) => (
-                    <SwiperSlide key={index} className="flex flex-col gap-2 p-[16px] rounded-lg bg-white shadow-3xl">
-                      <CardGalleryTransportSkeleton />
-                    </SwiperSlide>
-                  ))
-                }
-
-            </Swiper>
-        </div>
-    )
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={12}
+        id="swiper-tour-recommendation"
+        navigation
+        modules={[Navigation]}
+        className="mySwiper !static mb-0 h-[22rem]"
+        breakpoints={{
+          0: {
+            slidesPerView: 1,
+          },
+          500: {
+            slidesPerView: 1.5,
+          },
+          768: {
+            slidesPerView: 2.5,
+          },
+          1024: {
+            slidesPerView: 3.5,
+          },
+          1280: {
+            slidesPerView: 4.5,
+          },
+          1536: {
+            slidesPerView: 5,
+          },
+        }}
+      >
+        {/* CART TRANSPORT */}
+        {transportCatalogs.length > 0
+          ? transportCatalogs.map((transportData, index) => (
+              <SwiperSlide key={index} className="!rounded-lg">
+                <div className="flex flex-col gap-2 p-[16px] rounded-lg bg-white shadow-3xl">
+                  <CatalogueRoutesCard
+                    transport={transportData}
+                    recommended={true}
+                  />
+                </div>
+              </SwiperSlide>
+            ))
+          : [...Array(5)].map((_, index) => (
+              <SwiperSlide
+                key={index}
+                className="flex flex-col gap-2 p-[16px] rounded-lg bg-white shadow-3xl"
+              >
+                <CardGalleryTransportSkeleton />
+              </SwiperSlide>
+            ))}
+      </Swiper>
+    </div>
+  );
 }
