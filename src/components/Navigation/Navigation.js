26f8b673ddfbedec4156c9d33cc23ea3aee4949e
LuisPerazaV <160287372+LuisPerazaV@users.noreply.github.com>
2024-08-29 09:42:39 -0500

    banner selfie

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,7 +7,7 @@ export default function Navigation({hotelDetails=false}) {
   return (
     <div className={`${hotelDetails ? 'static' : 'static' } flex flex-col top-0 z-[9]`}>
       <img
-        src={`${process.env.NEXT_PUBLIC_URL}img/home/line-navigation.jpeg`}
+        src={`${process.env.NEXT_PUBLIC_URL}img/home/line-navigation.jpg`}
         alt={`${process.env.NEXT_PUBLIC_NAME_COMPANY} line orange`}
         height={8}
         className="size-full"
