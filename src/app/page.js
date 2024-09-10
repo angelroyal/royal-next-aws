b9b9910ae09cae10d7ffebba5d990bb7e98ea592
Angel Bastian <abastian@36t.mx>
2024-09-10 11:13:19 -0600

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 export const revalidate = 60;
 
 import axios from "axios";
+import { cookies } from "next/headers";
 import BannersHeaderHome, {
   BannersHomeExclusiveDiscounts,
   BannersHomeOffers,
@@ -28,8 +29,12 @@ import { ImageProvider } from "@/context/ImageContext";
 
 export default async function DetailPageHotel({ params }) {
   try {
+    const cookiesStore = cookies();
+    const language = cookiesStore.get("language")?.value || "en";
+    console.log(language);
+
     const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_API_CRM}getImages/all`,
+      `${process.env.NEXT_PUBLIC_API_CRM}getImages/${language}/all`,
       {
         headers: {
           "Cache-Control":
@@ -79,7 +84,7 @@ export default async function DetailPageHotel({ params }) {
                   <PopularDestinationsHome />
                   <ChainsHome />
                   <EnjoyStayHome />
-                  <TransportBanner/>
+                  <TransportBanner />
 
                   <WaveLine />
                   <BannerDiscoverPossibilities />
@@ -94,7 +99,7 @@ export default async function DetailPageHotel({ params }) {
       </ImageProvider>
     );
   } catch (error) {
-    console.log("error img",error);
+    console.log("error img", error);
 
     return (
       <LanguageProvider>
--- src/language/LanguageProvider.jsx
+++ src/language/LanguageProvider.jsx
@@ -1,29 +1,37 @@
 "use client";
 
+import Cookies from "js-cookie";
 import  { useEffect, useState } from "react";
-import LanguageContext from "./LanguageContext";
+
 import { es } from "./es";
 import { en } from "./en";
+import LanguageContext from "./LanguageContext";
+
 
 function LanguageProvider(props) {
   const [language, setLanguage] = useState("en");
-  const [currency, setCurrency] =useState("MXN");
+  const [currency, setCurrency] = useState("MXN");
 
-  useEffect(()=>{
-    const storedLanguage = localStorage.getItem("language") || "en";
+  useEffect(() => {
+    const storedLanguage = Cookies.get("language") || "en";
     setLanguage(storedLanguage);
-    
+
     const currencyData = sessionStorage.getItem("selectedCurrency");
     setCurrency(currencyData);
-  },[])
+  }, []);
+
+  const handleSetLanguage = (lang) => {
+    setLanguage(lang);
+    Cookies.set("language", lang);
+  };
 
   const languageData = language === "es" ? es : en;
 
   const value = {
     language,
-    setLanguage,
+    setLanguage: handleSetLanguage,
     languageData,
-    currency
+    currency,
   };
 
   return (
