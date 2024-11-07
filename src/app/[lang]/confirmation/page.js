53570ee2d96a775de61c0cc34f8bc6e3f75d4302
abastian <abastian@36t.mx>
2024-11-07 09:07:36 -0600

    ficed

--- src/app/[lang]/confirmation/page.js
+++ src/app/[lang]/confirmation/page.js
@@ -1,11 +1,11 @@
-import ConfirmReservation from '@/payment/Confirmation/ConfirmReservation'
+// import ConfirmReservation from '@/payment/Confirmation/ConfirmReservation'
 import React from 'react'
 
 export default function page() {
   return (
     <div>AQUI VA LA PANTALLA DE CONFIRMACION 
 
-        <ConfirmReservation/>
+        {/* <ConfirmReservation/> */}
     </div>
   )
 }
