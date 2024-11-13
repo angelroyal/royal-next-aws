ca779743071b6bbd72572200e8811ed9746f34a8
abastian <abastian@36t.mx>
2024-11-13 09:27:49 -0600

    routes

--- src/app/[lang]/test/page.js
+++
@@ -1,9 +0,0 @@
-import AlertGeneric from "@/components/Alerts/LottiePay/AlertGeneric";
-import AlertProcessing from "@/components/Alerts/LottiePay/AlertProcessing";
-import FooterT from "@/components/Footer/FooterT";
-AlertProcessing
-import FutureDates from "@/hooks/Test2";
-import React from "react";
-export default function page() {
-  return <AlertProcessing/>;
-}
